9e03d4f351d26dd744a2640b88b0bb2749e705c5
Ankit Raj <radheankit110031@gmail.com>
2026-03-14 20:23:05 +0530

    home screen basic done

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:amicons/amicons.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -11,8 +12,19 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text('WhatsApp', style: TextStyle(fontWeight: FontWeight.bold)),
+        elevation: 0, // normal elevation
+        scrolledUnderElevation: 0, // when scrolling
+        surfaceTintColor: Colors.transparent, // disables material tint
+        title: Text(
+          'WhatsApp',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.green,
+            fontSize: 26,
+          ),
+        ),
         actions: [
           FaIcon(FontAwesomeIcons.qrcode),
           const SizedBox(width: 25),
@@ -24,27 +36,28 @@ class HomeScreen extends StatelessWidget {
             child: FaIcon(
               FontAwesomeIcons.circleUser,
               size: 30,
-              color: Colors.black54,
+              color: Colors.black,
             ),
           ),
           const SizedBox(width: 10),
         ],
-        backgroundColor: Colors.green,
-        foregroundColor: Colors.white,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
       ),
       body: Column(
         children: [
           Container(
-            color: Colors.green,
+            height: 60,
+            color: Colors.white,
             child: Padding(
               padding: EdgeInsetsGeometry.symmetric(
-                vertical: 10,
+                vertical: 5,
                 horizontal: 10,
               ),
               child: Container(
                 decoration: BoxDecoration(
                   color: Colors.white30,
-                  borderRadius: BorderRadius.circular(20),
+                  borderRadius: BorderRadius.circular(40),
                 ),
                 child: TextField(
                   decoration: InputDecoration(
@@ -64,25 +77,57 @@ class HomeScreen extends StatelessWidget {
               ),
             ),
           ),
-          SizedBox(height: 10),
+          // SizedBox(height: 10),
           Expanded(
             child: ListView.builder(
-              itemCount: 20,
+              itemCount: 100,
               itemBuilder: (context, index) {
                 return Container(
                   color: Colors.black12,
-                  padding: EdgeInsets.symmetric(vertical: 5),
-                  margin: EdgeInsets.only(bottom: 10),
+                  padding: EdgeInsets.symmetric(vertical: 2),
+                  margin: EdgeInsets.only(top: 5),
                   child: ListTile(
                     leading: CircleAvatar(
-                      backgroundColor: Colors.black38,
+                      backgroundColor: Colors.white,
                       radius: 30,
+                      child: FaIcon(
+                        FontAwesomeIcons.solidCircleUser,
+                        size: 55,
+                        color: Colors.blueGrey,
+                      ),
+                    ),
+                    title: Text(
+                      'Users ${index + 1}',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text('last message'),
+                    trailing: Column(
+                      children: [
+                        Text('12:10 PM', style: TextStyle(fontSize: 13)),
+                        SizedBox(height: 10),
+                        if (index % 5 == 0)
+                          FaIcon(FontAwesomeIcons.bellSlash, size: 20),
+                      ],
                     ),
                   ),
                 );
               },
             ),
           ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.chat), label: "Chats"),
+          BottomNavigationBarItem(
+            icon: Icon(Amicons.vuesax_status, fontWeight: FontWeight.bold),
+            label: "Status",
+          ),
+          // BottomNavigationBarItem(
+          //   icon: Icon(Icons.groups),
+          //   label: "Communities",
+          // ),
+          BottomNavigationBarItem(icon: Icon(Icons.call), label: "Calls"),
         ],
       ),
     );
